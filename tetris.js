23b784a2b51f3f480f01a4029563931c40a32f4f
vernonkjr <vkerrjr@gmail.com>
2019-09-02 17:24:09 -0400

    pieces with different color

--- tetris.js
+++ tetris.js
@@ -42,38 +42,38 @@ const createMatrix = (w, h) => {
         ];
      } else if (type === 'O') {
         return [
-            [1, 1],
-            [1, 1]
+            [2, 2],
+            [2, 2]
         ];
      } else if (type === 'L') {
         return [
-           [0, 1, 0],
-           [0, 1, 0],
-           [0, 1, 1]
+           [0, 3, 0],
+           [0, 3, 0],
+           [0, 3, 3]
        ];
     } else if (type === 'J') {
         return [
-           [0, 1, 0],
-           [0, 1, 0],
-           [1, 1, 0]
+           [0, 4, 0],
+           [0, 4, 0],
+           [4, 4, 0]
        ];
     } else if (type === 'I') {
         return [
-           [0, 1, 0, 0],
-           [0, 1, 0, 0],
-           [0, 1, 0, 0],
-           [0, 1, 0, 0]
+           [0, 5, 0, 0],
+           [0, 5, 0, 0],
+           [0, 5, 0, 0],
+           [0, 5, 0, 0]
        ];
     } else if (type === 'S') {
         return [
-           [0, 1, 1],
-           [1, 1, 0],
+           [0, 6, 6],
+           [6, 6, 0],
            [0, 0, 0]
        ];
     } else if (type === 'Z') {
         return [
-           [1, 1, 0],
-           [0, 1, 1],
+           [7, 7, 0],
+           [0, 7, 7],
            [0, 0, 0]
        ];
     }
@@ -93,7 +93,7 @@ const drawMatrix = (matrix, offset) => {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value !== 0) {
-                context.fillStyle = 'red';
+                context.fillStyle = colors[value];
                 context.fillRect(x + offset.x, y + offset.y, 1, 1);
             }
         });
@@ -192,13 +192,23 @@ const update = (time = 0) => {
     requestAnimationFrame(update);
 }
 
+const colors = [
+    null,
+    'red',
+    'blue',
+    'violet',
+    'green',
+    'purple',
+    'orange',
+    'pink'
+];
+
 const arena = createMatrix(12, 20);
 
 const player = {
     pos: {x: 5, y: 5},
     matrix: createPiece('T')
 };
-
 
 document.addEventListener('keydown', event => {
     if (event.keyCode === 37) {
